c115e4ba6e28569787ceba36949fb5a547a7b529
Ben Purinton <ben.purinton@gmail.com>
2023-02-17 14:41:35 -0900

    add note to count.rb

--- count.rb
+++ count.rb
@@ -1,11 +1,12 @@
-# Create an Array and use the count method to print 
-# how many elements are in the Array.
-# 
-# Expected output (with input "4 10"):
+# Ask the user to enter at least 2 numbers and store their input.                                                                          
+# The starting code below does this for you.                                                                                               
+# Use the `count` method to prtin how many numbers the user entered.                                                                       #                                                                                                                                          
 #
-#   "Enter at least 2 numbers, separated by spaces:"
-#   4 10
-#   2
+# Expected output (with input "42 5 3 8"):                                                                                                                         
+#
+#   "Enter at least 2 numbers, separated by spaces:"                                                                                       
+#   42 5 3 8 # user's input                                                                                                                
+#   4 
 
 p "Enter at least 2 numbers, separated by spaces:"
 
